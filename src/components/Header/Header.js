c2383d9bc3a1474f3960d28a6745293a4f443129
mgulzar0202 <mgulzar0202@gmail.com>
2023-08-23 12:41:11 +0400

    search function added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,14 @@ import { SearchBar } from './SearchBar';
 import Notifications from './NotificationBar/Notifications';
 import Messages from './NotificationBar/Messages';
 import Profile from "./Profile"
+import pdfIcon from '../../../src/pictures/icons/pdf.png'
+import docxIcon from '../../../src/pictures/icons/word.png'
+import txtIcon from '../../../src/pictures/icons/txt.png'
+import cssIcon from '../../../src/pictures/icons/css.png'
+import jsonIcon from '../../../src/pictures/icons/json.png'
+import htmlIcon from '../../../src/pictures/icons/html.png'
+import mp4Icon from '../../../src/pictures/icons/video.png'
+import jpegIcon from '../../../src/pictures/icons/img.png'
 
 export function Header() {
   const [isNotificationsOpen, setIsNotificationsOpen] = useState(false);
@@ -11,21 +19,124 @@ export function Header() {
   const toggleNotificationsBar = () => {
     setIsNotificationsOpen(!isNotificationsOpen);
   };
+  const [filteredFiles, setFilteredFiles] = useState([]);
+  const allFilesList = [
+    {
+      "id": 1,
+      "name": "For kid development.pdf",
+      "size": 10.45,
+      "type": "PDF File",
+      "icon": pdfIcon,
+      "url": "https://drive.google.com/file/d/1_hqtRaZmjIMuu3j6dAABjZABT2CytiL5/view?usp=drive_link"
+    },
+    {
+      "id": 2,
+      "name": "Marathon.docx",
+      "size": 22.67,
+      "type": "Word Document",
+      "icon": docxIcon,
+      "url": "https://docs.google.com/document/d/19vte6wBcsFWfLyd0yzF_Z2D-YMdwUQWv/edit?usp=drive_link&ouid=116780887794393565861&rtpof=true&sd=true"
+    },
+    {
+      "id": 3,
+      "name": "robots.txt",
+      "size": 43.9,
+      "type": "Text Document",
+      "icon": txtIcon,
+      "url": "https://drive.google.com/file/d/1jSYyOuQU-HXqUVN7Km-iqWEOEXRRYb9O/view?usp=drive_link"
+    },
+    {
+      "id": 4,
+      "name": "Docs for VISA.docx",
+      "size": 22.67,
+      "type": "Word Document",
+      "icon": docxIcon,
+      "url": "https://docs.google.com/document/d/166VFvQn3yYhw0q4BNbDe3d8xzN9JEa7L/edit?usp=drive_link&ouid=116780887794393565861&rtpof=true&sd=true"
+    },
+    {
+      "id": 5,
+      "name": "package.json",
+      "size": 2.7,
+      "type": "JSON File",
+      "icon": jsonIcon,
+      "url": "https://drive.google.com/file/d/1Zjhx7IynK2PFASeQE78GB86bOFD7znma/view?usp=drive_link"
+    },
+    {
+      "id": 6,
+      "name": "App.css",
+      "size": 42.67,
+      "type": "CSS File",
+      "icon": cssIcon,
+      "url": "https://drive.google.com/file/d/1ZWs5UG67fOQDIhhRgYUzg4p6EUmqMFPM/view?usp=drive_link"
+    },
+    {
+      "id": 7,
+      "name": "index.html",
+      "size": 42.67,
+      "type": "HTML Document",
+      "icon": htmlIcon,
+      "url": "https://drive.google.com/file/d/1JSPLnkQTo9DdNudQi2PWttl7azs2bPCE/view?usp=drive_link"
+    },
+    {
+      "id": 10,
+      "name": "about.txt",
+      "size": 42.67,
+      "type": "Text Document",
+      "icon": txtIcon,
+      "url": "https://drive.google.com/file/d/1txCYNH5WhzXXdwsbToOjExVT6I3i63vY/view?usp=drive_link"
+    },
+    {
+      "id": 11,
+      "name": "Make the pair.pdf",
+      "size": 42,
+      "type": "PDF File",
+      "icon": pdfIcon,
+      "url": "https://drive.google.com/file/d/1_QGQOtf9hf-y3Q0U9EtycCp6N72ZVz6l/view?usp=drive_link"
+    },
+    {
+      "id": 8,
+      "name": "Dog spa.mp4",
+      "size": 24.40,
+      "type": "Mp4 File",
+      "icon": mp4Icon,
+      "url": "https://drive.google.com/file/d/1z6M-xJGS_bZGzmaw2LY58D8-m8uU7pHp/view?usp=drive_link"
+    },
+    {
+      "id": 9,
+      "name": "4.jpg",
+      "size": 4.40,
+      "type": "JPG File",
+      "icon": jpegIcon,
+      "url": "https://drive.google.com/file/d/13FXmQzcR9rLBGTrCp52llIbf39P0rCzQ/view?usp=drive_link"
+    }
+  ]
+  const handleSearchTextChange = (searchText) => {
+    console.log('Search text changed:', searchText);
+
+    const filteredResults = allFilesList.filter((file) =>
+      file.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setFilteredFiles(filteredResults);
+  };
 
   return (
     <div className="header">
       <div className="logo">
-        <a href='//src/index.js'>do<span>ar</span> </a>
+        <a href='/src/index.js'>do<span>ar</span> </a>
       </div>
       <div className="nav-menu">
-        <SearchBar />
+        <SearchBar onTextChange={handleSearchTextChange} filteredFiles={filteredFiles}/>
       </div>
       <div className="notification-bar">
-      <Notifications toggleNotificationsBar={toggleNotificationsBar} open={isNotificationsOpen} />
-      <Messages toggleNotificationsBar={toggleNotificationsBar} open={isNotificationsOpen} />
-      <Profile toggleNotificationsBar={toggleNotificationsBar} open={isNotificationsOpen}/>
+        <Notifications toggleNotificationsBar={toggleNotificationsBar} open={isNotificationsOpen} />
+        <Messages toggleNotificationsBar={toggleNotificationsBar} open={isNotificationsOpen} />
+        <Profile toggleNotificationsBar={toggleNotificationsBar} open={isNotificationsOpen} />
       </div>
       <div className="div">
+       {filteredFiles.map((file) => (
+                    <div key={file.id} className="filtered-file">
+                    </div>
+                ))}
       </div>
     </div>
   );
--- src/components/Header/SearchBar.js
+++ src/components/Header/SearchBar.js
@@ -1,20 +1,47 @@
 import '../../App.css';
 import { useTranslation } from 'react-i18next';
+import React, { useState } from 'react';
 
-export function SearchBar({ onTextChange }) {
+export function SearchBar({ onTextChange, filteredFiles }) {
     const { t } = useTranslation();
+    const [clickedFile, setClickedFile] = useState(null);
+    const [inputText, setInputText] = useState('');
+
+    const handleTextChange = (searchText) => {
+        setInputText(searchText);
+        onTextChange(searchText);
+    };
+
+    const isInputEmpty = filteredFiles.length === 0;
 
     return (
         <div className="search">
             <div className='search-bar'>
-            <i className="fa-solid fa-magnifying-glass"></i>
-                <input 
-                 type="text"
-                 onChange={(e) => onTextChange(e.target.value)} 
-                 placeholder={t('placeholder.searchFilesAndFolders')}/>
+                <i className="fa-solid fa-magnifying-glass"></i>
+                <input
+                    type="text"
+                    value={inputText}
+                    onChange={(e) => handleTextChange(e.target.value)}
+                    placeholder={t('placeholder.searchFilesAndFolders')} />
             </div>
+            {!isInputEmpty && filteredFiles.length > 0 && clickedFile === null && (
+                <div className="search-results">
+                    {filteredFiles.map((file) => (
+                        <div
+                            key={file.id}
+                            className={`filtered-file ${clickedFile === file.id ? 'active' : ''}`}
+                            onClick={() => {
+                                setClickedFile(file.id);
+                                setInputText('');
+                            }}>
+                            <img src={file.icon} alt={file.type} />
+                            <a href={file.url} target="_blank" rel="noopener noreferrer">
+                                {file.name}
+                            </a>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
-
-
